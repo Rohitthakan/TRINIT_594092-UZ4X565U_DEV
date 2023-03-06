f989b7aafe9b9b2b0776184ee660a881d1e44431
Rohitthakan <110251143+Rohitthakan@users.noreply.github.com>
2023-03-06 21:32:12 +0530

    Update urls.py

--- goodcauses/urls.py
+++ goodcauses/urls.py
@@ -11,13 +11,16 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('add', views.about, name='about'),
     path('resources', views.services, name='services'),
-    path('login', views.login, name='login'),
+    path('login', views.user_login, name='login'),
+    path('logout', views.user_logout, name='logout'),
     path('signup', views.signup, name='signup'),
     path('profile', views.profile, name='profile'),
     path('list', views.list, name='list'),
     path('dash', views.dash, name='dash'),
     path('feedback', views.feedback, name='feedback'),
-    path('fundraising', views.fundraising, name='fundraising')
+    path('fundraising', views.fundraising, name='fundraising'),
+    path('forgot_password', views.forgot_password, name='forgot_password'),
+    path('http://127.0.0.1:8000/reset_password/<uidb64>/<token>', views.reset_password, name='reset_password'),
 ]
 
 if settings.DEBUG:
