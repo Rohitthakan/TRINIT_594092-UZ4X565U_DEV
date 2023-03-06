7e1889b19b74cf8081043aa15255eca0ca0a90e6
Rohitthakan <110251143+Rohitthakan@users.noreply.github.com>
2023-03-06 21:38:52 +0530

    Update settings.py

--- tri_nit_hackathon/settings.py
+++ tri_nit_hackathon/settings.py
@@ -86,6 +86,14 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    # 'django.contrib.auth.hashers.BcryptSHA256PasswordHasher',
+    # 'django.contrib.auth.hashers.BcryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -128,3 +136,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'collegerepo2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'prapektbkduxmtpg'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
