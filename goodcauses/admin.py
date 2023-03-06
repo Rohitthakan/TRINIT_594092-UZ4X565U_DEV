1c5feb9f012cc51ba8d058197c8f96455d8a4b13
Rohitthakan <110251143+Rohitthakan@users.noreply.github.com>
2023-03-06 21:32:57 +0530

    Delete goodcauses directory

--- goodcauses/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from goodcauses.models import Signin, Profile, Feedback, Funds
-
-# Register your models here.
-admin.site.register(Signin)
-admin.site.register(Profile)
-admin.site.register(Feedback)
-admin.site.register(Funds)
